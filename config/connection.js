9e7a013a44a232c73357aaa0cd61b9dce5784950
w.hunt <wfh2d88@gmail.com>
2021-03-23 10:29:58 -0500

    initialize

---
+++ config/connection.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+// create connection to our db
+let sequelize;
+
+
+module.exports = sequelize;
---
+++ server.js
@@ -0,0 +1,9 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
