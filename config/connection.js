fac061dd5f7d4774e3113cd985ad82e01c8faeea
w.hunt <wfh2d88@gmail.com>
2021-03-24 17:08:40 -0500

    .env working

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize('its_a_techBlog_db', 'root', 'Iaabmc88', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
