72efad57d1f22742ba0832aab69913ee50ee818d
w.hunt <wfh2d88@gmail.com>
2021-03-28 16:22:41 -0500

    heroku

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 const homeRoutes = require('./home-routes.js');
 const apiRoutes = require('./api');
 const dashboardRoutes = require('./dashboard-routes.js');
+const userRoutes = require('./api/user-routes.js')
 const router = require('express').Router();
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use('/api/users', userRoutes);
 
 
 module.exports = router;
