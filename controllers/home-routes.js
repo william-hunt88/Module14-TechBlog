1185af0e1367c1044e6774db6e8440af6cbff11b
w.hunt <wfh2d88@gmail.com>
2021-04-05 12:40:48 -0500

    heroku

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -36,13 +36,13 @@ router.get("/", (req, res) => {
   });
 });
 
-// router.get("/login", (req, res) => {
-//   if (req.session.loggedIn) {
-//     res.redirect("/login");
-//     return;
-//   }
-//   res.render("login");
-// });
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
 
 router.get("/post/:id", (req, res) => {
   Post.findOne({
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -44,6 +44,7 @@ async function loginFormHandler(event) {
       }),
       headers: { "Content-Type": "application/json" },
     });
+    
     if (response.ok) {
       document.location.replace("/dashboard");
     } else {
